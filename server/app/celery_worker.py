588b2b1e3305a365e8ddc44706e188ea0013a182
sergiy <kukaletssergiy@gmail.com>
2025-05-26 19:47:13 +0300

    Added celery, celery_beat configurations

--- server/app/celery_worker.py
+++ server/app/celery_worker.py
@@ -1,10 +1,16 @@
+# app/celery_worker.py
 import os
 from celery import Celery
-# from app.core.config import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
+from app.core.config import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
+from app.celery_beat_schedule import CELERY_BEAT_SCHEDULE
 
-celery = Celery(__name__)
-celery.conf.broker_url = os.getenv("CELERY_BROKER_URL")
-celery.conf.result_backend = os.getenv("CELERY_RESULT_BACKEND")
+celery = Celery(__name__,
+broker = CELERY_BROKER_URL,
+backend = CELERY_RESULT_BACKEND
+)
+
+celery.conf.beat_schedule = CELERY_BEAT_SCHEDULE
+celery.conf.timezone = 'UTC'  # Set the timezone for Celery tasks
 
 # Automatically detect tasks in 'app' applications
 celery.autodiscover_tasks(['app'])
